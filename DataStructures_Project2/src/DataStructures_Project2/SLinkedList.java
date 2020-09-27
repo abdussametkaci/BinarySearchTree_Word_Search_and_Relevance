24e690598f1bf4d4748eba1b8751c6fbf28a6d30
abdussametkaci <61049743+abdussametkaci@users.noreply.github.com>
2020-09-27 12:36:15 +0300

    Update SLinkedList.java

--- DataStructures_Project2/src/DataStructures_Project2/SLinkedList.java
+++ DataStructures_Project2/src/DataStructures_Project2/SLinkedList.java
@@ -4,7 +4,7 @@
  * icerisinde nodeList tutar sirali bir sekilde
  * @assignment Odev 2
  * @date 13.05.2020
- * @author Abdussamet KACI abdussamet.kaci@stu.fsm.edu.tr
+ * @author Abdussamet KACI
  */
 package DataStructures_Project2;
 
