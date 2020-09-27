e12141fd8e3790f1bc7b0a0aef4a5a7f3c040d87
abdussametkaci <61049743+abdussametkaci@users.noreply.github.com>
2020-09-27 12:35:48 +0300

    Update Node.java

--- DataStructures_Project2/src/DataStructures_Project2/Node.java
+++ DataStructures_Project2/src/DataStructures_Project2/Node.java
@@ -4,7 +4,7 @@
 * yazilmis programdir
 * @assignment Odev 2
 * @date 13.05.2020
-* @author Abdussamet KACI abdussamet.kaci@stu.fsm.edu.tr
+* @author Abdussamet KACI
 */ 
 package DataStructures_Project2;
 
