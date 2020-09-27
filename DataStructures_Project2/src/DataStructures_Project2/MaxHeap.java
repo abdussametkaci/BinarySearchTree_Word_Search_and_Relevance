123d1b062a28f77dc3886f4ed6505572372b7f61
abdussametkaci <61049743+abdussametkaci@users.noreply.github.com>
2020-09-27 12:35:17 +0300

    Update MaxHeap.java

--- DataStructures_Project2/src/DataStructures_Project2/MaxHeap.java
+++ DataStructures_Project2/src/DataStructures_Project2/MaxHeap.java
@@ -3,7 +3,7 @@
  * @description Bu program Max Heap yapisini olusturmaktadir. 
  * @assignment Odev 2
  * @date 13.05.2020
- * @author Abdussamet KACI abdussamet.kaci@stu.fsm.edu.tr
+ * @author Abdussamet KACI
  */
 package DataStructures_Project2;
 
